3b4016c20c6ce70e0416864113f96de1e3ff52a1
Soroush81@gmail.com <soroush81@gmail.com>
2022-01-02 16:50:28 +0330

    changes

--- src/main/java/ir/soodeh/mancala/MancalaApplication.java
+++ src/main/java/ir/soodeh/mancala/MancalaApplication.java
@@ -1,9 +1,11 @@
 package ir.soodeh.mancala;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+//disable the white label error page to see our custom error
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class MancalaApplication {
 
     public static void main(String[] args) {
